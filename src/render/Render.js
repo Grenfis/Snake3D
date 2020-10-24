1fb2fc71075b36220cef54ee307b579acff0a82d
Viktor Kubli <viktor.kubli@gmail.com>
2020-10-24 16:08:08 +1100

    Выпилил gridhelper и контроллер камеры

--- src/render/Render.js
+++ src/render/Render.js
@@ -1,6 +1,5 @@
 import * as ThreeJs from "three";
 import Config from "../Config";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 export default class Render {
     constructor() {
@@ -11,11 +10,9 @@ export default class Render {
             Config.render.viewport.far);
         this.renderer = new ThreeJs.WebGLRenderer();
         this.renderQueue = [];
-        this.gridHelper = new ThreeJs.GridHelper(100, 10);
 
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.light = new ThreeJs.DirectionalLight(0xffffff, 1);
-        this.sky = new ThreeJs.AmbientLight(0xffffff, 2);
+        this.light = new ThreeJs.DirectionalLight(0xffffff, .5);
+        this.sky = new ThreeJs.AmbientLight(0xffffff, 3);
 
         this.init();
     }
@@ -27,20 +24,14 @@ export default class Render {
     }
 
     render() {
-        this.light.position.set(
-            this.camera.position.x,
-            this.camera.position.y,
-            this.camera.position.z
-        );
+        this.light.position.copy(this.camera.position);
 
         const scene = new ThreeJs.Scene();
-        scene.add(this.gridHelper);
         scene.add(this.light);
         scene.add(this.light.target);
         scene.add(this.sky);
         this.renderQueue.forEach(obj => scene.add(obj.getMesh()));
 
-        this.controls.update();
         this.renderer.render(scene, this.camera);
         this.renderQueue = [];
     }
