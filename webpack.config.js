6009b9846e007743dd0e788ba17cbef654810e99
Viktor Kubli <viktor.kubli@gmail.com>
2020-10-24 12:20:06 +1100

    Добавил babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public', 'dist'),
         filename: 'bundle.js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
 }
