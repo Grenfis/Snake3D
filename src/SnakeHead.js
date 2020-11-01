575b1ba6b358690d9299a6d93c31837880ff4efc
Viktor Kubli <viktor.kubli@gmail.com>
2020-11-01 18:14:08 +1100

    Закоментил код класса головы змеи

--- src/SnakeHead.js
+++ src/SnakeHead.js
@@ -1,11 +1,20 @@
 import Cube from "./Cube";
 
 export default class SnakeHead extends Cube {
+    /**
+     *
+     * @param {Object} data
+     * @param {Camera} camera
+     */
     constructor(data, camera) {
         super(data);
         this.camera = camera;
     }
 
+    /**
+     * Переопределение Cube.getPosition, тк голова в другой системе координат
+     * @return {Vector3}
+     */
     getPosition() {
         const localPosition = this.mesh.position;
         let position = localPosition.clone();
